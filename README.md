a8eb103af21290a0f2865fbfb4a1517db2bf6920
cyyeh <chihyu.jimmy.yeh@gmail.com>
2020-06-15 09:33:58 +0800

    init

---
+++ README.md
@@ -0,0 +1,4 @@
+# 11-785 Introduction to Deep Learning, Spring 2020
+
+course website: https://deeplearning.cs.cmu.edu/S20/index.html
+
---
+++ assignments/hw3/hw3p1/hw3/mc.py
@@ -0,0 +1,13 @@
+# You know the drill...
+
+def question_1():
+    raise NotImplemented
+
+def question_2():
+    raise NotImplemented
+
+def question_3():
+    raise NotImplemented
+
+def question_4():
+    raise NotImplemented
